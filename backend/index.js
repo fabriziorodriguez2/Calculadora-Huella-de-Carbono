109c1f0b83e3753deb2b11f6c322c1ab08f409fa
fabriziorodriguez2 <fabrizio.rodriguez@correo.ucu.edu.uy>
2025-05-20 14:20:16 -0300

    minor fix: Move frontend files to backend/frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -13,21 +12,9 @@ const respuestasRoutes = require('./routes/respuestas.routes');
 app.use('/api', authRoutes);
 app.use('/api', respuestasRoutes);
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
-app.get('*', (req, res) => {
-  const indexPath = path.join(__dirname, 'frontend', 'menu.html'); // o index.html si tuvieras uno
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res.status(404).send('Archivo no encontrado');
-  }
-});
-
 app.get('/', (req, res) => {
   res.send('API de Huella de Carbono funcionando');
 });
-
 
 app.listen(process.env.PORT,'0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
